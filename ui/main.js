81c234c68d06d60a61d116c38e4b3c2a35963c57
Lallu98 <lalithalakshmiramesh@gmail.com>
2017-08-15 16:02:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //To change the content of main text
 var element = document.getElementById("main text");
 element.innerHTML = 'New value';
+
+//move the image
+var img = document.getElementById("madi");
+img.onclick = function() {
+    img.style.marginleft = '100px';
+};
