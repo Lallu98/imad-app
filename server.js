aa85984b06abd6a677d3a79a4560175f4a17dfc9
Lallu98 <lalithalakshmiramesh@gmail.com>
2017-08-20 01:55:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ var htmlTemplate=`
                ${heading}
             </h3>
             <div>
-                ${date.toDateString();}
+                ${date.toDateString()}
             </div>
             <div>
                 ${content}
